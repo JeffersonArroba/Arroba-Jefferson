b2fce49d6aa833042d1ad57f46eaeb4391fe3d27
JeffersonArroba <gokuarroba@gmail.com>
2021-10-29 18:51:11 -0500

    Mi actividad A1

--- Arroba Jefferson.cpp
+++ Arroba Jefferson.cpp
@@ -2,6 +2,6 @@
 using namespace std;
 int main()
 {
-cout<<"Hola mi nombre es Jefferson Joel Arroba Charcopa";
+cout<<"Hola mi nombre es Jefferson Joel Arroba Charcopa y estoy en el paralebo B";
 return 0;
 }
